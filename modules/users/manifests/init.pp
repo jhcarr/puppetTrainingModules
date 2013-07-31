17d45234ff1f25f2fd6b391c1bbba46b959584d9
justin <justin@puppetlabs.vm>
2013-07-31 00:20:53 +0000

    Minor changes to test class use from console

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,10 +1,13 @@
 class users {
   user  { 'fundamentals':
     ensure => present,
-    gid    => 'staff',
+    gid    => 'guest',
     shell  => '/bin/bash',
   }
   group { 'staff':
+    ensure => present,
+  }
+  group { 'guest':
     ensure => present,
   }
 }
